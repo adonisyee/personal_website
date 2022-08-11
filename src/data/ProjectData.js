e7952bfe5e8082c882e9f22f449e33a6cdd76953
adonisyee <adonisyee@berkeley.edu>
2022-08-11 01:35:35 -0700

    added projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -6,15 +6,51 @@ export const data = [
         "src": "https://grandmas-recipes-app.herokuapp.com/",
         "title": "Grandma's Recipes",
         "description": "A website I created to save family recipes.",
-        "tags": "Javascript, HTML, CSS, Bootstrap, MongoDB, EJS, Node, Express, front-end, back-end"
+        "tags": "Javascript, EJS, CSS, Bootstrap, MongoDB, Node, Express, front-end, back-end"
     },
     {
         "key": 1,
+        "image": "../img/travel-journal.png",
+        "alt": "Travel Journal Preview",
+        "src": "https://adonisyee-travel-journal.herokuapp.com/",
+        "title": "Travel Journal",
+        "description": "A travel journal I made to document where I've been.",
+        "tags": "Javascript, React"
+    },
+    {
+        "key": 2,
+        "image": "../img/Tenzies.png",
+        "alt": "Tenzies Game Preview",
+        "src": "https://tenzies-game-project.herokuapp.com/",
+        "title": "Tenzies",
+        "description": "A strategic dice game!",
+        "tags": "Javascript, React"
+    },
+    {
+        "key": 3,
         "image": "../img/Quizzical.png",
         "alt": "Quizzical App Preview",
         "src": "https://quizzical-project-react.herokuapp.com/",
         "title": "Quizzical Project",
-        "description": "A simple but fun react application I built to practice state and api queries.",
+        "description": "A trivia quiz game!",
         "tags": "Javascript, React, APIs"
+    },
+    {
+        "key": 4,
+        "image": "../img/speed-typing-game.png",
+        "alt": "Speed Typing Game Preview",
+        "src": "https://speed-typing-game-project.herokuapp.com/",
+        "title": "Speed Typing Game",
+        "description": "A speed-typing game!",
+        "tags": "Javascript, React"
+    },
+    {
+        "key": 5,
+        "image": "../img/business-card.png",
+        "alt": "Business Card Preview",
+        "src": "https://adonisyee-business-card.herokuapp.com/",
+        "title": "Virtual Business Card",
+        "description": "A virtual business card I made for myself.",
+        "tags": "Javascript, React"
     }
 ]
