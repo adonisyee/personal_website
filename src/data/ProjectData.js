2676226f7a006b3bd556a05a2c9c946449318bfc
adonisyee <adonisyee@berkeley.edu>
2023-01-02 12:02:39 -0800

    update recipe website url

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -3,7 +3,7 @@ export const data = [
         "key": 0,
         "image": "../img/grandmas_recipes.JPG",
         "alt": "Grandma's Recipes Project Preview",
-        "src": "https://grandmas-recipes-app.herokuapp.com/",
+        "src": "https://grandmas-recipes.onrender.com/",
         "title": "Grandma's Recipes",
         "description": "A website I created to save family recipes.",
         "tags": "Javascript, EJS, CSS, Bootstrap, MongoDB, Node, Express, RESTful APIs, front-end, back-end"
