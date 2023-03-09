9e91ae9718396ff08e27d3c779baebf8e85c7664
adonisyee <adonisyee@berkeley.edu>
2023-03-09 10:42:14 -0800

    update about pics

--- src/data/AboutImages.js
+++ src/data/AboutImages.js
@@ -2,16 +2,26 @@ export const data = [
     {
         key: 0,
         image: "../img/1.JPG",
-        description: "An image"
+        description: "Me in Lake Tahoe"
     },
     {
         key: 1,
         image: "../img/2.JPG",
-        description: "An image"
+        description: "Me with socks from my company"
     },
     {
         key: 2,
+        image: "../img/5.JPG",
+        description: "Me at a restaurant"
+    },
+    {
+        key: 3,
+        image: "../img/4.JPG",
+        description: "Me with Yugioh friends"
+    },
+    {
+        key: 4,
         image: "../img/3.JPG",
-        description: "An image"
+        description: "Me in Joshua Tree"
     }
 ]
